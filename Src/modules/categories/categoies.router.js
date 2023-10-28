526ec253b63b08e9bc1371b1623cd946d39bd0a9
Hanan Twair <hanantwair168@gmail.com>
2023-10-28 22:56:21 +0200

    update category module

--- Src/modules/categories/categoies.router.js
+++ Src/modules/categories/categoies.router.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import * as categoriesContoller from './categories.controller.js';
-import fileUpload,{fileValidation} from '../../services/multer.js'
+import fileUpload, { fileValidation } from '../../services/multer.js'
 const router = Router();
 
 router.get('/', categoriesContoller.getCategories);
+router.get('/active', categoriesContoller.getActiveCategory);
+router.get('/:id', categoriesContoller.getSpecificCategory);
+
 router.post('/', fileUpload(fileValidation.image).single('image'), categoriesContoller.createCategory);
+router.patch('/:id', fileUpload(fileValidation.image).single('image'), categoriesContoller.updateCategory);
 
 export default router;
